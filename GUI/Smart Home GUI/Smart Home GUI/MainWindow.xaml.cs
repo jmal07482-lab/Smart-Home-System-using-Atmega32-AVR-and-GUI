9510af44ecdc36d97718d63071809fd760f7f514
Ahmed Mostafa <ahmedabubakr60@gmail.com>
2025-11-17 18:59:56 +0200

    Add files via upload

---
+++ GUI/Smart Home GUI/Smart Home GUI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Smart_Home_GUI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smart_Home_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainWindowViewModel();
+            DataContext = ViewModel;
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            ViewModel.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}

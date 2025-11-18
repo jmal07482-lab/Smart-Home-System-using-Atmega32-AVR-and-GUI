e8d20b3124dcb249758376b0f5019fb70c049d6c
Ahmed Gamal Abdulhameed <jmal07482@gmail.com>
2025-11-18 10:56:10 -0800

    Add files via upload

---
+++ LIB/Bit_Math.h
@@ -0,0 +1,17 @@
+/*
+ * Bit_Math.h
+ *
+ * 
+ *      Author: Ahmed Gamal
+ */
+
+#ifndef LIB_BIT_MATH_H_
+#define LIB_BIT_MATH_H_
+
+#define SET_BIT(reg,bit)   ((reg)|=(1<<(bit)))
+#define TOG_BIT(reg,bit)   ((reg)^=(1<<(bit)))
+#define CLR_BIT(reg,bit)   ((reg)&=(~(1<<(bit))))
+#define READ_BIT(reg,bit)  (((reg)&(1<<(bit)))>>(bit))
+
+#endif /* LIB_BIT_MATH_H_ */
+

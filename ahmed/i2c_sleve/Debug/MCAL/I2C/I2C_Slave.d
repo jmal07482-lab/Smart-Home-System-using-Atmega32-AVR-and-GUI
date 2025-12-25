13eab13f7fcc7a0b53a6628ed02619bf803d38a2
Ahmed Gamal Abdulhameed <jmal07482@gmail.com>
2025-12-24 23:16:37 -0800

    Delete ahmed directory

--- ahmed/i2c_sleve/Debug/MCAL/I2C/I2C_Slave.d
+++
@@ -1,9 +0,0 @@
-MCAL/I2C/I2C_Slave.o MCAL/I2C/I2C_Slave.o: ../MCAL/I2C/I2C_Slave.c \
-  ../MCAL/I2C/I2C_Slave.h ../MCAL/I2C/../../LIB/STD_TYPES.h \
-  ../MCAL/I2C/../../LIB/BIT_MATH.h
-
-../MCAL/I2C/I2C_Slave.h:
-
-../MCAL/I2C/../../LIB/STD_TYPES.h:
-
-../MCAL/I2C/../../LIB/BIT_MATH.h:

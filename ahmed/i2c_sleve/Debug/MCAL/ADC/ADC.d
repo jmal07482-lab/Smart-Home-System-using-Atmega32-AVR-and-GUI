7155d4c7d7b0c745cd51d785678216641cdaeaf4
Ahmed Gamal Abdulhameed <jmal07482@gmail.com>
2025-12-24 23:16:21 -0800

    Delete ahmed directory

--- ahmed/i2c_sleve/Debug/MCAL/ADC/ADC.d
+++
@@ -1,9 +0,0 @@
-MCAL/ADC/ADC.o MCAL/ADC/ADC.o: ../MCAL/ADC/ADC.c \
-  ../MCAL/ADC/../../LIB/BIT_MATH.h ../MCAL/ADC/../../LIB/STD_TYPES.h \
-  ../MCAL/ADC/ADC.h
-
-../MCAL/ADC/../../LIB/BIT_MATH.h:
-
-../MCAL/ADC/../../LIB/STD_TYPES.h:
-
-../MCAL/ADC/ADC.h:
